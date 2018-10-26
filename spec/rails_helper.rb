efc238a1c0b8e322b719087a04de79ae7c1b1a5a
Zak <zakary.howard94@gmail.com>
2018-10-26 16:18:37 +0200

    coverage added to gitignore, cucumberm rauks and spec helper require coveralls

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,11 @@
+require 'coveralls'
+Coveralls.wear_merged!('rails')
+
 require 'spec_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
+
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
@@ -10,6 +15,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
